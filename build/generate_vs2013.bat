71d205804525b276a2a3733ba41c15ffafb3f905
Andrei Klimchuk <andrei.klimchuk@vml.com.au>
2016-08-22 14:11:48 +1000

    cmake for qt

--- build/generate_vs2013.bat
+++
@@ -1,4 +0,0 @@
-call "D:\Programs\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-call "C:\Program Files (x86)\VS Professional 2013\VC\vcvarsall.bat"
-rem start cmake-gui.exe
-cmake -G "Visual Studio 12 2013 Win64" %cd%/..
--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,25 @@
 #include "tutorial3.hpp"
 #include "tutorial4.hpp"
 
-int main(void) {
+/*int main(void) {
     //auto app = Tutorial1::Create();
     //auto app = Tutorial2::Create();
     //auto app = Tutorial3::Create();
     auto app = Tutorial4::Create();
     app->run();
     delete app;
+}*/
+
+#include <QApplication>
+#include "mainwindow.hpp"
+
+int main(int argc, char *argv[])
+{
+    //Q_INIT_RESOURCE(application);
+
+    QApplication app(argc, argv);
+
+    MainWindow mainWin;
+    mainWin.show();
+    return app.exec();
 }
